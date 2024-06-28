fdda717b0b193e0637da54f02bc46aab95d0a184
ermannos <e.scanagatta@gmail.com>
2024-06-28 11:37:47 +0200

    Fixed type export

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,7 @@ type zeroToNine = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 type YYYY = `19${zeroToNine}${zeroToNine}` | `20${zeroToNine}${zeroToNine}`;
 type MM = `0${oneToNine}` | `1${0 | 1 | 2}`;
 type DD = `${0}${oneToNine}` | `${1 | 2}${zeroToNine}` | `3${0 | 1}`;
-type ISODateString = `${YYYY}-${MM}-${DD}`;
+export type ISODateString = `${YYYY}-${MM}-${DD}`;
 
 export interface Measure {
   date: ISODateString;
